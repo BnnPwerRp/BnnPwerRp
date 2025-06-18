3ddf8694368f319189ff637c8ab1c3c42c72928a
BnnPwerRp <antoine2001@live.ca>
2025-06-18 14:54:40 -0400

    Update config.js

--- sleekload/config.js
+++ sleekload/config.js
@@ -4036,7 +4036,7 @@ config.music_random_order = true
 
 // Music volume
 // Choose a value between 0 (quietest) and 100 (loudest)
-config.music_volume = 42
+config.music_volume = 6
 
 // Use Gmod volume for the music instead of manual volume
 config.music_gmod_volume = true
