4c882d26f1f407af573029da03095e72bef23bf9
BnnPwerRp <antoine2001@live.ca>
2025-06-18 15:07:16 -0400

    Update config.js

--- sleekload/config.js
+++ sleekload/config.js
@@ -4039,7 +4039,7 @@ config.music_random_order = true
 config.music_volume = 4
 
 // Use Gmod volume for the music instead of manual volume
-config.music_gmod_volume = true
+config.music_gmod_volume = false
 
 // Enable custom messages?
 // "true" or "false"
